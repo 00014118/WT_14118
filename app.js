44ec4155d4fcf2f623a64287b1baecae71d06434
eosheie <eosheie@gmail.com>
2023-04-03 00:15:09 +0500

    finishing

--- app.js
+++ app.js
@@ -1,17 +1,90 @@
 const express = require ('express')
 const app = express()
 
+const fs = require('fs')
+
+
 
 app.set('view engine' , 'pug')
 
-app.use ('/static', express.static('public'))
+app.use('/static', express.static('public'))
+app.use(express.urlencoded({ extended: false }))
+
 //localhost 
 app.get('/', (req, res) => {
     res.render('home')
 })
+
+app.get ('/create',(req, res) =>
+{
+    res.render('create')})
+
+app.post ('/create', (req, res) => {
+    const title = req.body.title
+    const content = req.body.content
+
+    if (title.trim() === '' && content.trim() === '') {
+        res.render('create', {error: true});
+    } else {
+        fs.readFile('./data/notes.json', (err, data) =>{
+            if (err) throw err
+
+            const notes =JSON.parse(data)
+
+            notes.push ({
+                id: id (),
+                title: title,
+                content:content,
+                })
+
+            fs.writeFile('./data/notes.json', JSON.stringify(notes), err => {
+                if(err) throw err
+
+                res.render('create', {success :true } )
+                })
+            })
+        }
+}) 
+
+
+    
+app.get('/notes', (req, res) => {
+
+    fs.readFile('./data/notes.json', (err, data) => {
+        if (err) throw err
+
+        const notes = JSON.parse(data)
+
+        res.render('notes', { notes: notes })
+
+    })
+    
+})
+
+app.get('/notes/:id', (req, res) => {
+    const id = req.params.id
+
+    fs.readFile('./data/notes.json', (err, data) => {
+        if (err) throw err
+
+        const notes = JSON.parse(data)
+
+        const note = notes.filter(note => note.id == id)[0]
+
+        res.render('detail', { note: note})
+    })
+
+    
+})
+
+
 
 app.listen(8000, err => {
     if (err)console.log(err)
 
     console.log('Server is running on port 8000')
 })
+
+function id () {
+    return'_' + Math.random().toString(36).substring(2, 9);
+}
