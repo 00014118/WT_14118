7259e0fa4e7334343fcedf1fb56bdf24ac3889aa
eosheie <eosheie@gmail.com>
2023-04-03 00:14:33 +0500

    finish

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require ('express')
+const app = express()
+
+
+app.set('view engine' , 'pug')
+
+app.use ('/static', express.static('public'))
+//localhost 
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.listen(8000, err => {
+    if (err)console.log(err)
+
+    console.log('Server is running on port 8000')
+})
